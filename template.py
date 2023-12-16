035007e26aad82ac06200b6c923b9c786101ab5e
Krishan Walia <krishanw30@gmail.com>
2023-12-16 14:12:56 +0530

    Requirements Added

--- template.py
+++ template.py
@@ -21,21 +21,25 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
+    "templates/index.html",
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
     
-    # 
+    # If there is no folder (file directory) for the file to be created
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory: {filedir} for the file: {filename} ")
     
+    # If the file does not exists or the file is empty => Create a file at file path
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,"w") as f:
-            logging.info(f"Creating empty file : {filepath}")
             pass
+            logging.info(f"Creating empty file : {filepath}")
+    
+    # Else log that the file already exists
     else:
         logging.info(f"{filename} is already existing")
 
