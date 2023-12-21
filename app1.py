d515ad5e1995317241f94342e9fa2acf82c77b6d
Krishan Walia <krishanw30@gmail.com>
2023-12-21 19:49:04 +0530

    Streamlit Configurations Added

---
+++ app1.py
@@ -0,0 +1,49 @@
+import os
+import streamlit as st
+from cnnClassifier.pipeline.predict import PredictionPipeline
+import io
+from PIL import Image
+
+# Client App for saving the filepath
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputImage.jpg"
+        self.classifier = PredictionPipeline(self.filename)
+
+
+# For running the complete pipeline
+def main():
+
+    # giving a title to the streamlit website
+    st.title("Check for the Chicken Coccidiosis Disease")
+    # st.markdown("*Streamlit* is **really** ***cool***.")
+
+    # Getting the input image file from the user
+    uploaded_image = st.file_uploader("Select the Image (JPG Format): ", type= "jpg",accept_multiple_files=False)
+
+    if uploaded_image:
+        # Converting the image to bytes
+        bytes_data_image = uploaded_image.getvalue()
+
+        # Converting the bytes back to image
+        image_conv = Image.open(io.BytesIO(bytes_data_image))
+
+        # Saving the image to the filepath specified in clApp.filename
+        image_conv.save(clApp.filename)
+
+        # Creating a button for prediction
+        if st.button("Disease Test Result"):
+            result = clApp.classifier.predict()
+
+            st.success(result)
+        if st.button("Train"):
+            os.system("dvc repro")
+            st.success("Training Successful!")
+
+
+
+if __name__ == "__main__":
+    clApp = ClientApp()
+    main()
+    
+
